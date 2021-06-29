01a6677dfc17eac333b1d0fe35ee79331e70282a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 13:13:14 +0100

    Update Post.java

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,5 +1,15 @@
 package com.example.demo.model;
 
-public class Post extends AuditModel {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="posts")
+public class Post extends AuditModel {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
 }
