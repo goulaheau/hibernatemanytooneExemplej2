05eb2d7e2f536aafe8f919db6fe7495ae310964f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 13:09:40 +0100

    Update AuditModel.java

--- src/main/java/com/example/demo/model/AuditModel.java
+++ src/main/java/com/example/demo/model/AuditModel.java
@@ -1,8 +1,33 @@
 package com.example.demo.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+		value = {"createdAt","updatedAt"},
+		allowGetters = true
+		)
 public abstract class AuditModel implements Serializable {
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at",nullable = false,updatable = false)
+	@CreatedDate
+	private Date createdAt;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="updated_at",nullable = false)
 	
+	private Date updatedAt;
 
 }
