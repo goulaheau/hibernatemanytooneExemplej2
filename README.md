671efc37029efaa6b8ee44dfa6c1e7be0ef3532c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 12:57:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hibernatemanytooneExemplej2
+ 
---
+++ src/main/java/com/example/demo/HibernatemanytooneExemplej2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatemanytooneExemplej2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatemanytooneExemplej2Application.class, args);
+	}
+
+}
